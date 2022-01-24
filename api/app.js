ccdc9a6d2dd0a72b48135650b4b421946d79881b
Ge0h <george.walker.wme@gmail.com>
2022-01-24 10:25:46 +0000

    Refactor: Added npmpackages dotenv & concurrently to enable PORT to be saved in config file and run servers concurrently.

--- api/app.js
+++ api/app.js
@@ -1,15 +1,20 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var testAPIRouter = require("./routes/testAPI");
+// config dotenv
+dotenv.config({ path: __dirname + "/config/config.env" });
 
-var app = express();
+// router files
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const testAPIRouter = require("./routes/testAPI");
+
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
